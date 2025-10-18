bb1b270e46f0e421bac83d10a2475bcbe477338c
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-10-18 15:24:44 +0530

    received data from app and todolist components through props

--- to-do-app/src/components/ToDoItem.jsx
+++ to-do-app/src/components/ToDoItem.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 
+// prop destructuring with default value for type
 export default function ToDoItem({ data, onDelete, onComplete, onPending, onEdit, type }) {
+    // state to manage editing mode and edited task value
   const [isEditing, setIsEditing] = useState(false);
+//   state to manage the edited task text
   const [editedTask, setEditedTask] = useState(data);
 
+  // Save edited task
   function handleSave() {
     if (editedTask.trim() === '') return;
     onEdit(editedTask);
@@ -12,7 +16,9 @@ export default function ToDoItem({ data, onDelete, onComplete, onPending, onEdit
 
   return (
     <div className="item">
+        {/* edit the task using turnary operator */}
       <div className="task">
+        {/* new input field for editing task */}
         {isEditing ? (
           <input
             type="text"
@@ -26,15 +32,17 @@ export default function ToDoItem({ data, onDelete, onComplete, onPending, onEdit
 
       {type === 'todo' && (
         <div className="controls">
+            {/* buttons for different actions */}
           {!isEditing ? (
             <>
-              <button className="pending" onClick={onPending}>Do-Later</button>
+              <button className="pending" onClick={onPending}>Do Later</button>
               <button className="complete" onClick={onComplete}>Complete</button>
               <button className="delete" onClick={onDelete}>Delete</button>
               <button className="edit" onClick={() => setIsEditing(true)}>Edit</button>
             </>
           ) : (
             <>
+            {/* buttons for save and cancel editing */}
               <button className="save" onClick={handleSave}>Save</button>
               <button className="cancel" onClick={() => setIsEditing(false)}>Cancel</button>
             </>
@@ -44,3 +52,4 @@ export default function ToDoItem({ data, onDelete, onComplete, onPending, onEdit
     </div>
   );
 }
+
